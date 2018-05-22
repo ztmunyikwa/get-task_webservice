6f12ac53acbb86319aaad9a591e6671e274941b8
zanele <zanele@mit.edu>
2018-05-21 22:24:50 -0400

    return random element with industry... set_seed 22

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
-#import foo
-
-
+import pickle
+import json
+import pandas as pd
+import numpy as np
+import random 
 
 
 # Normal flask app stuff
@@ -12,30 +14,37 @@ app = Flask(__name__)
 
 #use the route() decorator to tell Flask what URL should trigger our function
 @app.route('/', methods=['POST'])
-def homepage():
+def homepage():	
+	df = pd.read_csv('occsDWAsIndustries_full.csv', sep=',')
+
+	np.random.seed(22)
+	get_random_industry_num = np.random.randint(0,df.shape[0])
+
+	#randomly select an industry for testing
+	#industry = df.get_value(get_random_industry_num, 'NAICS2')
+	#use payload to take input on industry
 	payload=request.get_json()
 	industry = payload['ind']
 
-	if industry==1:
-		task = "Make a Cake"
-		job= "Baker"
-	else:
-		task = "Plant a flower"
-		job= "Gardener"
+
+	df_industry= df[(df.NAICS2==industry)]
+
+	random.seed( 22 )
+	random_dwa =df_industry.sample(n=1) 
+
+	dwa_title = random_dwa.iloc[0,3]
+	job = random_dwa.iloc[0, 1]
 
 	return jsonify({
- 		'task': task,
+ 		'task': dwa_title,
         'job': job
-	})   
+	})  
+
+
+
+
+
  
-
- #    if request.method == 'POST':
-	#     return jsonify({
-	#         'prediction': prediction
-	#     })       
-	# else:
- #        return show_the_login_form()
-
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
